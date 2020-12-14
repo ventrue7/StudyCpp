ace7983b502c376092d1a2efc762da19073373cf
FangzhouYu <ventrue26@gmail.com>
2020-12-14 12:47:33 -0600

    add more functionality

--- Week6/main.cpp
+++ Week6/main.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/framework.hpp>
+#define BOOST_TEST_DYN_LINK
 
+using namespace boost::unit_test_framework;
+
+enum Period {
+    Month = 1,
+    Year = 12
+};
+
+enum Month {
+    January   = 1,
+    February  = 2,
+    March     = 3,
+    April     = 4,
+    May       = 5,
+    June      = 6,
+    July      = 7,
+    August    = 8,
+    September = 9,
+    October   = 10,
+    November  = 11,
+    December  = 12,
+    Jan = 1,
+    Feb = 2,
+    Mar = 3,
+    Apr = 4,
+    Jun = 6,
+    Jul = 7,
+    Aug = 8,
+    Sep = 9,
+    Oct = 10,
+    Nov = 11,
+    Dec = 12
+};
 
 class Date {
 public:
     Date(int m, int y) {
-        try {
-            if (y < 1900 || y > 2200) {
-                throw std::runtime_error("year " + std::to_string(y) + " is out of bound [1900,2199]");
-            }
+        if (y < 1900 || y > 9999) {
+            throw std::runtime_error("year " + std::to_string(y) + " is out of bound [1900,9999]");
         }
-        catch (const std::exception &e) {
-            std::cout << e.what() << "\n";
+        if (m > 12 || m < 1) {
+            throw std::runtime_error("month " + std::to_string(m) + " is out of bound Jan to Dec");
         }
-
-        try {
-            if (m > 12 || m < 1) {
-                throw std::runtime_error("month " + std::to_string(m) + " is out of bound Jan to Dec");
-            }
-        }
-        catch (const std::exception &e) {
-            std::cout << e.what() << "\n";
-        }
-
         m_date = (y - 1900) * 12 + m;
+        //std::cout<<"m_date: "<<m_date<<std::endl;
     }
 
     Date(int date) : m_date(date) {
@@ -31,20 +55,20 @@ public:
 
     Date() {};
 
-    int year() const{
-        return (m_date%12!=0)?(m_date / 12 + 1900):(m_date / 12 + 1900-1);
+    int year() const {
+        return (m_date % 12 != 0) ? (m_date / 12 + 1900) : (m_date / 12 + 1900 - 1);
     }
 
-    int month() const{
-        return (m_date%12!=0)?m_date%12:12;
+    int month() const {
+        return (m_date % 12 != 0) ? m_date % 12 : 12;
     }
 
     static int getMinDate() {
-        return 1;       // Jan 1901
+        return 1;       // Jan 1900
     }
 
     static int getMaxDate() {
-        return 3600;    // Dec 2199
+        return 97200;    // Dec 9999
     }
 
     static Date minDate() {
@@ -58,31 +82,52 @@ public:
     }
 
     void checkDate(int &date) {
-        try {
-            if (date < getMinDate()) {
-                throw std::runtime_error("date is smaller than min date");
-            }
+        if (date < getMinDate()) {
+            throw std::runtime_error("date is smaller than min date");
         }
-        catch (const std::exception &e) {
-            std::cout << e.what() << "\n";
+        if (date > getMaxDate()) {
+            throw std::runtime_error("date is larger than max date");
         }
-        try {
-            if (date > getMaxDate()) {
-                throw std::runtime_error("date is larger than max date");
-            }
-        }
-        catch (const std::exception &e) {
-            std::cout << e.what() << "\n";
-        }
-
     }
 
     int getDate() const {
         return m_date;
     }
 
+    int getPeriod(const Period &p) const {
+        int temp_month;
+        switch (p) {
+            case Month:
+                temp_month = 1;
+                break;
+            case Year:
+                temp_month = 12;
+                break;
+            default:
+                throw std::runtime_error("unknown date unit");
+        }
+        return temp_month;
+    }
+
+
     Date &operator+=(int months) {
         int temp_date = m_date + months;
+        checkDate(temp_date);
+        m_date = temp_date;
+        return *this;
+    }
+
+    Date &operator+=(const Period &p) {
+        int temp_month = getPeriod(p);
+        int temp_date = m_date + temp_month;
+        checkDate(temp_date);
+        m_date = temp_date;
+        return *this;
+    }
+
+    Date &operator-=(const Period &p) {
+        int temp_month = getPeriod(p);
+        int temp_date = m_date - temp_month;
         checkDate(temp_date);
         m_date = temp_date;
         return *this;
@@ -134,6 +179,21 @@ public:
     }
 
 
+    Date operator+(const Period &p) const {
+        int temp_month = getPeriod(p);
+        Date temp(*this);
+        temp += temp_month;
+        return temp;
+    }
+
+    Date operator-(const Period &p) const {
+        int temp_month = getPeriod(p);
+        Date temp(*this);
+        temp -= temp_month;
+        return temp;
+    }
+
+
     friend bool operator<(const Date &d1, const Date &d2) {
         return (d1.getDate() < d2.getDate());
     }
@@ -163,58 +223,91 @@ private:
     int m_date;
 };
 
+class DateTest {
+public:
+    static void testConsistency() {
+        int minDate = Date::minDate().getDate() + 1,
+                maxDate = Date::maxDate().getDate();
 
-int main() {
+        int mold = Date(minDate - 1).month(),
+                yold = Date(minDate - 1).year();
 
+        for (int i = minDate; i <= maxDate; i++) {
 
-    int minDate = Date::minDate().getDate()+1,
-        maxDate = Date::maxDate().getDate();
+            Date t(i);
+            int serial = t.getDate();
 
-    int mold  = Date(minDate-1).month(),
-        yold  = Date(minDate-1).year();
+            // check serial number consistency
+            if (serial != i)
+                std::cout << "inconsistent date\n"
+                          << "original: " << i << "\n"
+                          << "original: " << serial << "\n";
 
-    for (int i=minDate; i<=maxDate; i++) {
+            int m = t.month(),
+                    y = t.year();
 
-        Date t(i);
-        int serial = t.getDate();
-
-        // check serial number consistency
-        if (serial != i)
-            std::cout<<"inconsistent date\n"
-                     <<"original: "<<i<<"\n"
-                     <<"original: "<<serial<<"\n";
-
-        int     m  = t.month(),
-                y  = t.year();
-
-        //std::cout<<"current date "<<i<<","<<m<<","<<y<<"\n";
-        //check if skipping any month
-        if(!( (m == mold+1) ||(m == 1 && mold == 12 && y == yold+1) ))
-            std::cout<<"wrong month or year of date increment: \n"
-                     << "date: " << i << "\n"
-                     << "month,year: "<< m << "," << y<< "\n";
-
+            //std::cout<<"current date "<<i<<","<<m<<","<<y<<"\n";
+            //check if skipping any month
+            if (!((m == mold + 1) || (m == 1 && mold == 12 && y == yold + 1)))
+                std::cout << "wrong month or year of date increment: \n"
+                          << "date: " << i << "\n"
+                          << "month,year: " << m << "," << y << "\n";
 
 
-        mold = m; yold = y;
+            mold = m;
+            yold = y;
 
-        // check month definition
-        if (m < 1 || m > 12)
-            std::cout<<"invalid month: \n"
-                     << "date:  " << i << "\n"
-                     << "month: " << m<< "\n";
+            // check month definition
+            if (m < 1 || m > 12)
+                std::cout << "invalid month: \n"
+                          << "date:  " << i << "\n"
+                          << "month: " << m << "\n";
 
-        // create the same date with a different constructor
-        Date s(m,y);
-        // check number consistency
-        serial = s.getDate();
-        if (serial != i)
-            std::cout<<"inconsistent date:\n"
-                     << "date: " << i << "\n"
-                     << "cloned date: " <<  serial << "\n"
-                     << "month,year: "<< m << "," << y<< "\n";
+            // create the same date with a different constructor
+            Date s(m, y);
+            // check number consistency
+            serial = s.getDate();
+            if (serial != i)
+                std::cout << "inconsistent date:\n"
+                          << "date: " << i << "\n"
+                          << "cloned date: " << serial << "\n"
+                          << "month,year: " << m << "," << y << "\n";
+        }
+
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    static void isoDates() {
+        Date d(1, 2006);
+        if (d.month() != January ||
+            d.year() != 2006) {
+            std::cout<<"Iso date failed\n"
+                     << " month:         " << d.month() << "\n"
+                     << " year:          " << d.year();
+        }
+        Date d0 = d;
+        Date d1 = ++d0;
+        Date d2 = d0++;
+        if(d1.month() != d1.month() || d1.year() != d2.year()|| d1!= d2)
+            std::cout<<"Pre-increment or Post-increment or is not correct\n";
+        Date d3 = d0+Month;
+        if(d3.month() != d0.month()+1 || d3.year() != d0.year())
+            std::cout<<"Add period month is not correct\n";
+        Date d4 = d0+Year;
+        if(d4.month() != d0.month() || d4.year() != d0.year()+1)
+            std::cout<<"Add period year is not correct\n";
+
+    }
+};
+
+boost::unit_test::test_suite* init_unit_test_suite(int /*argc*/, char* /*argv*/[])
+{
+    std::cout << "start test\n";
+    test_suite *suite = BOOST_TEST_SUITE("Date tests");
+    suite->add(BOOST_TEST_CASE(&DateTest::testConsistency));
+    suite->add(BOOST_TEST_CASE(&DateTest::isoDates));
+    framework::master_test_suite().add(suite);
     return 0;
 }
+
+
+
